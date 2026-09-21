22e3f751d112ef74bcabdd1fa46a6f056a790363
Jahid Haider <mdjahidhelale@gmail.com>
2026-09-21 09:33:38 +0600

    I deleted dsa.md and dsa.py file

--- dsa.py
+++ dsa.py
@@ -10,3 +10,40 @@ if n==reverse:
   print("Palindrome")
 else:
   print("Not palindrome")
+
+#Armstrong Number
+
+n=153
+num1=n
+total=0
+length=len(str(n))
+while num1>0:
+  digit=num1%10
+  total=total+(digit**length)
+  num1=num1//10
+if total==n:
+  print("Armstrong Number")
+else:
+  print("Not Armstrong")
+  
+  #Another Way
+  
+n=153
+num=n
+count=0
+while num>0:
+  digit1=num%10
+  count+=1
+  num=num//10
+num1=n
+total=0
+while num1>0:
+  digit=num1%10
+  total=total+(digit**length)
+  num1=num1//10
+if total==n:
+  print("Armstrong Number")
+else:
+  print("Not Armstrong")
+  
+
