dacffffff87ba8fb175439a7f085d098204eb829
Jahid Haider <mdjahidhelale@gmail.com>
2026-09-21 09:05:39 +0600

    Palindrome number

--- dsa.py
+++ dsa.py
@@ -1,9 +1,12 @@
-#reverse number
-n=2347
+#palindrome number
+n=131
 num=n
 reverse=0
 while num>0:
   digit=num%10
   reverse=reverse*10+digit
   num=num//10
-print(reverse)
+if n==reverse:
+  print("Palindrome")
+else:
+  print("Not palindrome")
